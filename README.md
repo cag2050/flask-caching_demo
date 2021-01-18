41bb1f3fe4db3d7620a206aca71acad1529eeb68
cag2050 <cag2050@163.com>
2021-01-18 21:06:44 +0800

    add: flask-caching

---
+++ README.md
@@ -0,0 +1,12 @@
+### 说明
+1. Flask-Caching 是 Flask-Cache 扩展（ https://github.com/thadeusb/flask-cache ）的派生（fork），旨在替代缺乏维护的后者。
+2. 为了区分程序运行环境，Flask提供了一个 FLASK_ENV 环境变量用来设置环境，默认为 production（生产）。在开发时，我们可以将其设为 development（开发），这会开启所有支持开发的特性。
+3. 
+
+### Flask-Cache 学习资料
+
+Flask-Cache 学习资料 | 说明
+--- | ---
+官方文档 | http://www.pythondoc.com/flask-cache/index.html
+Flask-Cache缓存使用小记 | https://heshangbuxitou.github.io/2017/11/21/flask-cache/
+Flask-Caching | https://flask-caching.readthedocs.io/en/latest/
--- app.py
+++ app.py
@@ -1,13 +1,31 @@
 from flask import Flask
-from flask_cache import Cache
+# from flask_cache import Cache
+# 可使用 flask_caching 代替 flask_cache
+from flask_caching import Cache
+import os
 
 app = Flask(__name__)
 
-cache = Cache(config={'CACHE_TYPE': 'simple'})
+# simple
+# cache = Cache(config={'CACHE_TYPE': 'simple'})
+
+# filesystem
+# '''
+basedirs = os.path.abspath(os.path.dirname(__file__))
+basedir = basedirs + '/cache'
+
+cache = Cache(config={
+    'CACHE_TYPE': 'filesystem',
+    'CACHE_DIR': basedir
+})
+# '''
+
+cache.init_app(app)
 
 
 @app.route('/test_cache')
-@cache.cached(timeout=5)
+# timeout 单位为秒
+@cache.memoize(timeout=5)
 def hello_world():
-    print('if cache，the second request do not print')
+    print('如果在缓存有效期内，此行不会输出')
     return 'Hello, World!'
